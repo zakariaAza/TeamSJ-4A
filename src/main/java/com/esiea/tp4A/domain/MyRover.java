3269c8c949289c0de743f030e4232e93a489b019
Sasi <sasi.govindar@gmail.com>
2020-02-25 13:12:31 +0100

    Test methods enabled

--- src/main/java/com/esiea/tp4A/domain/MyRover.java
+++ src/main/java/com/esiea/tp4A/domain/MyRover.java
@@ -1,8 +1,19 @@
 package com.esiea.tp4A.domain;
 
 public class MyRover implements MarsRover {
+
+    private Position position;
+
     public MyRover() {
     }
 
+    @Override
+    public Position move(String command) {
+        if(command.isEmpty()) return Position.of(0,0, Direction.NORTH);
+        char singleCommand = command.charAt(0);
+        if('f' == singleCommand){
+            position = position;
+        }
+    }
 }
 
