0e0a1f81256c25eb45bd9500017e5f5602c652c9
Shnx <denis.ulrick@hotmail.fr>
2020-03-28 17:42:21 +0100

    Test detection obstacles : 10 tests passed

--- src/test/java/com/esiea/tp4A/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/MarsRoverTest.java
@@ -49,8 +49,8 @@ public class MarsRoverTest {
         "30, 20, EAST, 'lf', 30, 21, NORTH",
         "-40, 10, WEST, 'lb', -40, 11, SOUTH",
         "25, 0, SOUTH, 'fflb', 24, -2, EAST"
-
     })
+
     void rover_limit_positions(int givenX, int givenY, Direction givenDirection, String command, int expectedX, int expectedY, Direction expectedDirection){
         Mars mars = new Mars(100, Stream.of(new PointXY(0,1)).collect(Collectors.toSet()));
         MarsRover marsRover = new MyRover(givenX,givenY, givenDirection, mars);
@@ -59,14 +59,22 @@ public class MarsRoverTest {
             .isEqualTo(List.of(expectedX, expectedY, expectedDirection));
     }
 
-
     @ParameterizedTest
     @CsvSource({
-        "0, 50, NORTH, 'f', 0, -49, NORTH",
-        "50, 0, NORTH, 'rf', -49, 0, EAST"
+        "0, 50, NORTH, 'f', 0, 50, NORTH", // Obstacle (0, -49)
+        "50, 0, NORTH, 'rf', 50, 0, EAST", // Obstacle (-49 , 0)
+        "0, 50, SOUTH, 'b', 0, 50, SOUTH", // Obstacle (0, -49)
+        "50, 0, SOUTH, 'lf', 50,0, EAST", // Obstacle (-49 , 0)
+        "0, 50, WEST, 'lf', 0, 50, SOUTH", // Obstacle (0, -49)
+        "50, 0, WEST, rrf, 50, 0, EAST", // Obstacle (-49 , 0)
+        "-40, 10, WEST, 'lb', -40, 10, SOUTH", // Obstacle (-40, 11)
+        "0, 50, EAST, rb, 0, 50, SOUTH", // Obstacle (0, -49)
+        "50, 0, EAST, 'f', 50, 0, EAST", // Obstacle (0, -49)
+        "25, 0, SOUTH, 'fflb', 25, -2, EAST" // Obstacle (24, -2)
     })
+
     void rover_detect_obstacles(int givenX, int givenY, Direction givenDirection, String command, int expectedX, int expectedY, Direction expectedDirection){
-        Mars mars = new Mars(100, Stream.of(new PointXY(0,1)).collect(Collectors.toSet()));
+        Mars mars = new Mars(100, Stream.of(new PointXY(0,-49),new PointXY(-49,0),new PointXY(0,49),new PointXY(-40,11),new PointXY(24,-2)).collect(Collectors.toSet()));
         MarsRover marsRover = new MyRover(givenX,givenY, givenDirection, mars);
         Position newPosition = marsRover.move(command);
         Assertions.assertThat(newPosition).as("rover_detect_obstacles").extracting(Position::getX,Position::getY,Position::getDirection)
@@ -76,7 +84,7 @@ public class MarsRoverTest {
 
     @Test
     void rover_laser_shoot(){
-        Mars mars = new Mars(100, Stream.of(new PointXY(0,5)).collect(Collectors.toSet()));
+        Mars mars = new Mars(10, Stream.of(new PointXY(0,5), new PointXY(0,6)).collect(Collectors.toSet()));
         MarsRover marsRover = new MyRover(0,0, Direction.NORTH, mars);
         Position newPosition = marsRover.move("sfffff");
         Assertions.assertThat(newPosition).as("rover_laser_shoot").extracting(Position::getX,Position::getY,Position::getDirection)
