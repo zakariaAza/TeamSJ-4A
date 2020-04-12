dfc3c6596b4b86314eea7e1e88187a44f5b0dcef
Zakaria Azakkour <zakaria.azakkour@outlook.fr>
2020-04-12 22:54:16 +0200

    LocalMap Test Added

---
+++ src/test/java/com/esiea/tp4A/LocalMapTest.java
@@ -0,0 +1,42 @@
+package com.esiea.tp4A;
+
+import com.esiea.tp4A.domain.*;
+import com.esiea.tp4A.roverApi.LocalMap;
+import com.esiea.tp4A.roverApi.TheGame;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LocalMapTest {
+
+    @Test
+    void LocalMap_test(){
+        Mars mars = new Mars(100, Stream.of(new Obstacle(0,1)).collect(Collectors.toSet()));
+
+        Set<MyRover> roverSet = new HashSet<>();
+        roverSet.add( new MyRover(new TheGame(mars), 0, 1, Direction.NORTH, 5, mars, ""));
+
+        Set<Obstacle> obstacleSet = new HashSet<>();
+        obstacleSet.add( new Obstacle(0,1) );
+
+        LocalMap LocalMap = new LocalMap( roverSet, obstacleSet ) ;
+        for(Obstacle obstacle : LocalMap.getObstacles()){
+            assertThat(obstacle.getX()).as("ObstaclePoint").isEqualTo(0);
+            assertThat(obstacle.getY()).as("ObstaclePoint").isEqualTo(1);
+        }
+
+        for(MyRover rover : LocalMap.getPlayers()){
+            assertThat(rover.getPosition().getX()).as("RoverPositionX").isEqualTo(0);
+            assertThat(rover.getPosition().getY()).as("RoverPositionY").isEqualTo(1);
+        }
+    }
+}
+
